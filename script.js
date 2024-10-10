51dff85bbe0e3f4b85c1e6801012eb52c81d2f5d
rajkumarkalirajan <rkalirajan@hawk.iit.edu>
2024-10-10 12:28:55 -0400

    uploaded indes script js and css files

---
+++ script.js
@@ -0,0 +1,18 @@
+function init(){
+//add your javascrip between these two lines of code
+		function myEventFunction() {
+			alert('Hello');
+		}
+		
+		var d4 = document.getElementById('div4');		
+		
+		d4.addEventListener('click', myEventFunction);
+		
+		d4.addEventListener('click, function(){
+			console.log('I was clicked');
+			d4.style.backgroundColor = '#C00';
+			d4.innerHTML = '<string>Goodbye</strong>';
+		});
+}
+
+window.addEventListener('load', init);
